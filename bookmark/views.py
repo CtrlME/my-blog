75d1e6cc6304de03a19a0ad4c3fd8d13125f233e
CtrlME <rlaxorud22@naver.com>
2018-12-10 15:33:59 +0900

    My Django

---
+++ bookmark/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import ListView, DetailView
+from bookmark.models import Bookmark
+
+class BookmarkLV(ListView):
+    model = Bookmark
+class BookmarkDV(DetailView):
+    model = Bookmark
